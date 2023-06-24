97c95bdad4b4c756bd2c0c5057b357417b91a658
João <joaovictor.ramos34@gmail.com>
2023-06-24 12:40:40 -0300

    Criação da page de SignIn

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,13 @@ import theme from "./styles/theme";
 import GlobalStyle from "./styles/global";
 import { ThemeProvider } from "styled-components";
 
-import { Home } from "./pages/Home";
+import { SignIn } from "./pages/SignIn";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Home />
+      <SignIn />
     </ThemeProvider>
   </React.StrictMode>
 );
