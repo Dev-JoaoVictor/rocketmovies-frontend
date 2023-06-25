b9eb4e79a11e319dfad27cc8809308d6d8bbe6f4
João <joaovictor.ramos34@gmail.com>
2023-06-25 14:05:55 -0300

    Componente Textarea

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,11 +1,26 @@
-import { Container } from "./styles";
+import { Container, Form } from "./styles";
+import { FiArrowLeft } from "react-icons/fi";
 
+import { Input } from "../../components/Input";
 import { Header } from "../../components/Header";
+import { TextArea } from "../../components/TextArea";
+import { ButtonText } from "../../components/ButtonText";
 
 export function New() {
   return (
     <Container>
       <Header />
+      <main>
+        <Form>
+          <ButtonText title="voltar" icon={FiArrowLeft} />
+          <h1>Novo filme</h1>
+          <div>
+            <Input placeholder="Título" type="text" />
+            <Input placeholder="Sua nota (de 0 a 5)" type="text" />
+          </div>
+          <TextArea placeholder="Observações" type="text"/>
+        </Form>
+      </main>
     </Container>
   );
 }
--- src/pages/New/styles.js
+++ src/pages/New/styles.js
@@ -10,4 +10,23 @@ export const Container = styled.div`
     "header"
     "content"
   ;
+
+  > main {
+    grid-area: content;
+    padding: 0 12.3rem;
+    margin: 4rem 0;
+  }
+`;
+
+export const Form = styled.form`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 2.4rem;
+
+  > div {
+    display: flex;
+    gap: 4rem;
+    justify-content: space-between;
+  }
 `;
