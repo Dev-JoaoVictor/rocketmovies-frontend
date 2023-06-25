fcfd70a8c27c6a69fe3f49446d47992094449396
João <joaovictor.ramos34@gmail.com>
2023-06-25 13:24:29 -0300

    Adicionando o cabeçalho a pagina de novo filme

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,13 @@ import theme from "./styles/theme";
 import GlobalStyle from "./styles/global";
 import { ThemeProvider } from "styled-components";
 
-import { Home } from "./pages/Home";
+import { New } from "./pages/New";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Home />
+      <New />
     </ThemeProvider>
   </React.StrictMode>
 );
