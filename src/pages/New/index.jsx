5c18b32d04906f06c7501c329ceda3a8334229d5
João <joaovictor.ramos34@gmail.com>
2023-07-03 11:52:17 -0300

    Adicionando tags

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,14 +1,24 @@
 import { Container, Form, ButtonDelete } from "./styles";
 import { FiArrowLeft } from "react-icons/fi";
 
+import { useState } from "react";
+
 import { Input } from "../../components/Input";
-import { Button } from "../../components/Button"
+import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 import { NoteItem } from "../../components/NoteItem";
 import { TextArea } from "../../components/TextArea";
 import { ButtonText } from "../../components/ButtonText";
 
 export function New() {
+  const [tags, setTags] = useState([]);
+  const [newTag, setNewTag] = useState("");
+
+  function handleAddtags() {
+    setTags((prevState) => [...prevState, newTag]);
+    setNewTag("");
+  }
+
   return (
     <Container>
       <Header />
@@ -20,17 +30,24 @@ export function New() {
             <Input placeholder="Título" type="text" />
             <Input placeholder="Sua nota (de 0 a 5)" type="text" />
           </div>
-          <TextArea placeholder="Observações" type="text"/>
+          <TextArea placeholder="Observações" type="text" />
           <p>Marcadores</p>
           <div className="tags">
-            <NoteItem value="React"/>
-            <NoteItem isNew placeholder="Novo marcador"/>
+            {tags.map((tag, index) => (
+              <NoteItem key={String(index)} value={tag} onClick={() => {}} />
+            ))}
+
+            <NoteItem
+              isNew
+              placeholder="Novo marcador"
+              onChange={(e) => setNewTag(e.target.value)}
+              value={newTag}
+              onClick={handleAddtags}
+            />
           </div>
           <div>
-            <ButtonDelete type="button">
-              Excluir filme
-            </ButtonDelete>
-            <Button type="button" title="Salvar alterações"/>
+            <ButtonDelete type="button">Excluir filme</ButtonDelete>
+            <Button type="button" title="Salvar alterações" />
           </div>
         </Form>
       </main>
